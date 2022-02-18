a2539d1d902931e764f357faec21252bcf78dd3e
Akansha Jain <jakansha2001@gmail.com>
2022-02-18 16:34:13 +0530

    complete user interface

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -18,76 +19,92 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
+          backgroundColor: Colors.black,
           body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                child: const Text('A'),
-                onPressed: () {
-                  playSound(1);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.amber,
-                  backgroundColor: Colors.red,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.amber,
+                    backgroundColor: Colors.red,
+                  ),
                 ),
               ),
-              TextButton(
-                child: const Text('B'),
-                onPressed: () {
-                  playSound(2);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.red,
-                  backgroundColor: Colors.orange,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.red,
+                    backgroundColor: Colors.orange,
+                  ),
                 ),
               ),
-              TextButton(
-                child: const Text('C'),
-                onPressed: () {
-                  playSound(3);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.red,
-                  backgroundColor: Colors.yellow,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.red,
+                    backgroundColor: Colors.yellow,
+                  ),
                 ),
               ),
-              TextButton(
-                child: const Text('D'),
-                onPressed: () {
-                  playSound(4);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.amber,
-                  backgroundColor: Colors.green,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.amber,
+                    backgroundColor: Colors.green,
+                  ),
                 ),
               ),
-              TextButton(
-                child: const Text('E'),
-                onPressed: () {
-                  playSound(5);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.amber,
-                  backgroundColor: Colors.teal,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.amber,
+                    backgroundColor: Colors.teal,
+                  ),
                 ),
               ),
-              TextButton(
-                child: const Text('F'),
-                onPressed: () {
-                  playSound(6);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.amber,
-                  backgroundColor: Colors.lightBlue,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.amber,
+                    backgroundColor: Colors.lightBlue,
+                  ),
                 ),
               ),
-              TextButton(
-                child: const Text('G'),
-                onPressed: () {
-                  playSound(7);
-                },
-                style: TextButton.styleFrom(
-                  primary: Colors.yellow,
-                  backgroundColor: Colors.purple,
+              Expanded(
+                child: TextButton(
+                  child: const Text(''),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  style: TextButton.styleFrom(
+                    primary: Colors.yellow,
+                    backgroundColor: Colors.purple,
+                  ),
                 ),
               ),
             ],
