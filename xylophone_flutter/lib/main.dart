b07328a3d0ed093fbfc72aa0c949b1a55c315947
Akansha Jain <jakansha2001@gmail.com>
2022-03-01 16:37:04 +0530

    optimized code

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -14,6 +14,23 @@ class MyApp extends StatelessWidget {
     player.play('assets_note$soundNumber.wav');
   }
 
+  Expanded buildKey({Color color = Colors.red, int soundNumber = 0}) {
+    return Expanded(
+      child: TextButton(
+        child: const Text(''),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: TextButton.styleFrom(
+          // primary: Colors.amber,
+          // backgroundColor: Colors.red,
+          primary: Colors.amber,
+          backgroundColor: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,91 +39,86 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.black,
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.amber,
-                    backgroundColor: Colors.red,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.red,
-                    backgroundColor: Colors.orange,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.red,
-                    backgroundColor: Colors.yellow,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.amber,
-                    backgroundColor: Colors.green,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.amber,
-                    backgroundColor: Colors.teal,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.amber,
-                    backgroundColor: Colors.lightBlue,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  child: const Text(''),
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  style: TextButton.styleFrom(
-                    primary: Colors.yellow,
-                    backgroundColor: Colors.purple,
-                  ),
-                ),
-              ),
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+              // Expanded(
+              //   child: TextButton(
+              //     child: const Text(''),
+              //     onPressed: () {
+              //       playSound(2);
+              //     },
+              //     style: TextButton.styleFrom(
+              //       primary: Colors.red,
+              //       backgroundColor: Colors.orange,
+              //     ),
+              //   ),
+              // ),
+              // Expanded(
+              //   child: TextButton(
+              //     child: const Text(''),
+              //     onPressed: () {
+              //       playSound(3);
+              //     },
+              //     style: TextButton.styleFrom(
+              //       primary: Colors.red,
+              //       backgroundColor: Colors.yellow,
+              //     ),
+              //   ),
+              // ),
+              // Expanded(
+              //   child: TextButton(
+              //     child: const Text(''),
+              //     onPressed: () {
+              //       playSound(4);
+              //     },
+              //     style: TextButton.styleFrom(
+              //       primary: Colors.amber,
+              //       backgroundColor: Colors.green,
+              //     ),
+              //   ),
+              // ),
+              // Expanded(
+              //   child: TextButton(
+              //     child: const Text(''),
+              //     onPressed: () {
+              //       playSound(5);
+              //     },
+              //     style: TextButton.styleFrom(
+              //       primary: Colors.amber,
+              //       backgroundColor: Colors.teal,
+              //     ),
+              //   ),
+              // ),
+              // Expanded(
+              //   child: TextButton(
+              //     child: const Text(''),
+              //     onPressed: () {
+              //       playSound(6);
+              //     },
+              //     style: TextButton.styleFrom(
+              //       primary: Colors.amber,
+              //       backgroundColor: Colors.lightBlue,
+              //     ),
+              //   ),
+              // ),
+              // Expanded(
+              //   child: TextButton(
+              //     child: const Text(''),
+              //     onPressed: () {
+              //       playSound(7);
+              //     },
+              //     style: TextButton.styleFrom(
+              //       primary: Colors.yellow,
+              //       backgroundColor: Colors.purple,
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
