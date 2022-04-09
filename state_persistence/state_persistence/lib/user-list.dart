15aa651d5fe76da0b4cd5631e25b05fc87ec7db3
Akansha Jain <jakansha2001@gmail.com>
2022-04-09 14:51:46 +0530

    Modified files

--- state_persistence/state_persistence/lib/user-list.dart
+++ state_persistence/state_persistence/lib/user-list.dart
@@ -34,32 +34,6 @@ String _age(Map<dynamic, dynamic> user) {
 }
 
 class _UserListState extends State<UserList> {
-  // ScrollController _scrollController = ScrollController();
-  // //List myList = [];
-  // int _currentMax = 10;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   //myList = List.generate(10, (index) => null);
-  //   _scrollController.addListener(() {
-  //     if (_scrollController.position.pixels ==
-  //         _scrollController.position.maxScrollExtent) {
-  //       _getMoreData();
-  //     }
-  //   });
-  // }
-
-  // _getMoreData() {
-  //   for (int i = _currentMax; i < _currentMax + 10; i++) {
-  //     //fetchUsers();
-  //   }
-
-  //   _currentMax = _currentMax + 10;
-
-  //   setState(() {});
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
